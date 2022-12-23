7c5a0b008f4c9746dd9960e1c6943d14335668ec
Regina Phalangy <nschallenberg@gmx.net>
2022-12-23 22:48:12 +0100

    Objects added to array in list variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 pokemonList = [
-    {},
-    {},
-    {},
-    {},
-    {}
+    {name: 'Farfetch\'d', height: 2.7, type: ['normal', 'flying']},
+    {name: 'Ivysaur', height: 3.3, type: ['grass', 'poison']},
+    {name: 'Sandile', height: 2.4, type: ['ground', 'dark']},
+    {name: 'Metagross', height: 5.3, type: ['steel', 'psychic']},
+    {name: 'Wyrdeer', height: 5.11, type: ['normal', 'psychic']},
+    {name: 'Toxtricity', heigth: 5.3, type: ['electric', 'poison']}
 ]
