53fcbaa8150645a2be64ae4c181441f7a352ea75
Regina Phalangy <nschallenberg@gmx.net>
2022-12-28 21:04:36 +0100

    Updated properties in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
-    {name: 'Farfetch\'d', height: 2.7, type: ['normal', 'flying']},
-    {name: 'Ivysaur', height: 3.3, type: ['grass', 'poison']},
-    {name: 'Sandile', height: 2.4, type: ['ground', 'dark']},
-    {name: 'Metagross', height: 5.3, type: ['steel', 'psychic']},
-    {name: 'Wyrdeer', height: 5.11, type: ['normal', 'psychic']},
-    {name: 'Toxtricity', heigth: 5.3, type: ['electric', 'poison']}
+    {name: 'Farfetch\'d', height: 2.7, types: ['normal', 'flying']},
+    {name: 'Ivysaur', height: 3.3, types: ['grass', 'poison']},
+    {name: 'Sandile', height: 2.4, types: ['ground', 'dark']},
+    {name: 'Metagross', height: 5.3, types: ['steel', 'psychic']},
+    {name: 'Wyrdeer', height: 5.11, types: ['normal', 'psychic']},
+    {name: 'Toxtricity', heigth: 5.3, types: ['electric', 'poison']}
 ]
