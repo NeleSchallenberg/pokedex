67b4b403b818e1b177ac343aaa87339fcca42464
Regina Phalangy <nschallenberg@gmx.net>
2022-12-29 11:21:52 +0100

    Added condition to loop in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,16 @@ let pokemonList = [
     {name: 'Sandile', height: 2.4, types: ['ground', 'dark']},
     {name: 'Metagross', height: 5.3, types: ['steel', 'psychic']},
     {name: 'Wyrdeer', height: 5.11, types: ['normal', 'psychic']},
-    {name: 'Toxtricity', height: 5.3, types: ['electric', 'poison']}
+    {name: 'Gyarados', height: 21, types: ['water', 'flying']}
 ]
 
 // Loop for displaying the pokemon list in browser.
 for (let i=0; i <= pokemonList.length; i++){
-    document.write
-        (`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`)
+
+    // Condition to highlight unsually big pokemons in the list
+    if (pokemonList[i].height >7){
+        document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!<br>');
+    } else {
+        document.write (`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`);
+    }
 }
