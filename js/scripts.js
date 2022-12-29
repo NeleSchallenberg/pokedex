b34921c62cd880cbb36555b22f382064623610bb
Regina Phalangy <nschallenberg@gmx.net>
2022-12-29 10:29:26 +0100

    Added loop to display pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
+
+// List of objects that describe different pokemon and their details.
 let pokemonList = [
     {name: 'Farfetch\'d', height: 2.7, types: ['normal', 'flying']},
     {name: 'Ivysaur', height: 3.3, types: ['grass', 'poison']},
     {name: 'Sandile', height: 2.4, types: ['ground', 'dark']},
     {name: 'Metagross', height: 5.3, types: ['steel', 'psychic']},
     {name: 'Wyrdeer', height: 5.11, types: ['normal', 'psychic']},
-    {name: 'Toxtricity', heigth: 5.3, types: ['electric', 'poison']}
+    {name: 'Toxtricity', height: 5.3, types: ['electric', 'poison']}
 ]
+
+// Loop for displaying the pokemon list in browser.
+for (let i=0; i <= pokemonList.length; i++){
+    document.write
+        (`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`)
+}
