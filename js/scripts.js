c7434f0227d8fb7b4ef0500236e3600af77fb77c
Regina Phalangy <nschallenberg@gmx.net>
2023-01-07 17:27:02 +0100

    Wrapped pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 
-// List of objects that describe different pokemon and their details.
-let pokemonList = [
-    {name: 'Farfetch\'d', height: 2.7, types: ['normal', 'flying']},
-    {name: 'Ivysaur', height: 3.3, types: ['grass', 'poison']},
-    {name: 'Sandile', height: 2.4, types: ['ground', 'dark']},
-    {name: 'Metagross', height: 5.3, types: ['steel', 'psychic']},
-    {name: 'Wyrdeer', height: 5.11, types: ['normal', 'psychic']},
-    {name: 'Gyarados', height: 21, types: ['water', 'flying']}
-]
+// List of objects that describe different pokemon and their details, wrapped in an IIFE.
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Farfetch\'d', height: 2.7, types: ['normal', 'flying']},
+        {name: 'Ivysaur', height: 3.3, types: ['grass', 'poison']},
+        {name: 'Sandile', height: 2.4, types: ['ground', 'dark']},
+        {name: 'Metagross', height: 5.3, types: ['steel', 'psychic']},
+        {name: 'Wyrdeer', height: 5.11, types: ['normal', 'psychic']},
+        {name: 'Gyarados', height: 21, types: ['water', 'flying']}
+    ]
+}) ();
 
 // ForEach loop 
 pokemonList.forEach(function(pokemon) {
