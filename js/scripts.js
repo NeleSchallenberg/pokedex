323d9ad797243f2def77375aef7855ca7031475b
Regina Phalangy <nschallenberg@gmx.net>
2023-01-13 12:56:41 +0100

    Appended list item and button

--- js/scripts.js
+++ js/scripts.js
@@ -33,40 +33,44 @@ pokemonRepository.add(
     {name: 'Pidgeot', height: 4.11, types: ['normal', 'water']}
 )
 
-// Updated forEach loop to retireve pokemonList array
-pokemonRepository.getAll().forEach(function(pokemon) {
-    // document.write (`<p> ${pokemon.name} (height ${pokemon.height}) </p>`);
 
-    let pokemonList = document.querySelector('ul');
+// Updated forEach loop to retrieve pokemonList array
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+
+    let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
+
     let button = document.createElement('button');
-        button.innerText = 'pokemon.name';
-        button.classList.add();
-    li.appendChild(button);
-    pokemonList.appendChild(li);
+        button.innerText = (pokemon.name);
+        button.classList.add('.button');
+
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
     }
 );
 
 
 
-
+// Old code from Exercise 1.5
+    // document.write (`<p> ${pokemon.name} (height ${pokemon.height}) </p>`);
 
 
 /*
 
 // Function with loop for displaying the pokemon list in browser.
-function printArrayDetails() {
-    for (let i=0; i <= pokemonList.length; i++){
+    function printArrayDetails() {
+        for (let i=0; i <= pokemonList.length; i++){
 
-        // Condition to highlight unsually big pokemons in the list
-        if (pokemonList[i]?.height > 7.9){
-            document.write (`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that\'s big!<br>`);
-        } else {
-            document.write (`${pokemonList[i]?.name} (height: ${pokemonList[i]?.height})<br>`);
+            // Condition to highlight unsually big pokemons in the list
+            if (pokemonList[i]?.height > 7.9){
+                document.write (`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that\'s big!<br>`);
+            } else {
+                document.write (`${pokemonList[i]?.name} (height: ${pokemonList[i]?.height})<br>`);
+            }
         }
     }
-}
 
-printArrayDetails();
+    printArrayDetails();
 
 */
